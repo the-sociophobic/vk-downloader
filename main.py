6b1447b15d8b22112d9252386b32429a83ef1f58
the-sociophobic <torikowashi@gmail.com>
2021-03-16 08:38:20 +0300

    fix download URL, rm unnecessary 'press Enter...'

--- main.py
+++ main.py
@@ -12,14 +12,13 @@ init(convert=True)
 
 
 def main():
-
+    
     clipboard = pyperclip.paste()
     if "vk.com" and "video" and "_" not in clipboard or clipboard is None:
         print()
         print("[" + Fore.RED + "-" + Style.RESET_ALL + "]" + " ERROR!!!\n"
               "Put video URL into your clipboard.\n"
               "URL Sample: https://vk.com/video1315763_456239439")
-        input("[#]" + " Press Enter to exit...")
         sys.exit(2)
     # converting part
     if "http://" in clipboard:
@@ -35,8 +34,7 @@ def main():
     content = page.read()
     page.close()
     link = content.decode('utf-8', "ignore")
-    string = re.compile('<source src=\\\\"([^"]*)\\\\"')
-    urls = string.findall(link)
+    urls = re.findall('<source src=\"([^"]*)\"', link)
 
     for i in ['1080.mp4', '720.mp4', '360.mp4', '240.mp4']:
         for url in urls:
@@ -51,17 +49,13 @@ def main():
                 fullpath = os.path.join(path, name)
                 if os.path.exists(fullpath):
                     print("[" + Fore.RED + "-" + Style.RESET_ALL + "] " + name + " already exists")
-                    input("[#]" + " Press Enter to exit...")
                     sys.exit(0)
-                input("[#]" + " Press Enter to start downloading...")
                 print("[" + Fore.GREEN + "+" + Style.RESET_ALL + "]" + " Downloading...")
                 urlretrieve(source, fullpath)
                 print("[" + Fore.GREEN + "+" + Style.RESET_ALL + "]" + " Saved as " + name)
-                input("[#]" + " Press Enter to exit...")
                 sys.exit(0)
 
     print("[" + Fore.RED + "-" + Style.RESET_ALL + "] " + "Can't find video.")
-    input("[#]" + " Press Enter to exit...")
     sys.exit(2)
 
 
